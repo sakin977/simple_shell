651812d3b82117014d942403188a4851a5f87eff
MavMarvin <marvinogwal@gmail.com>
2021-08-25 13:30:46 +0300

    Betty errors fixed

--- simplecode.c
+++ simplecode.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
- 
-int main () {
 
-   int a;
-	
-   /* for loop execution */
-   for( a = 0; a < 10; a = a + 1 ){
-      printf("value of a: %d\n", a);
-   }
- 
-   return 0;
+int main(void)
+{
+
+	int a;
+
+	/* for loop execution */
+	for (a = 0; a < 10; a = a + 1)
+	{
+		printf("value of a: %d\n", a);
+	}
+
+	return (0);
 }
